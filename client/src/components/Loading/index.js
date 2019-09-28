147a56551d365ba163aa439da8b42ee9b9d10db8
Walter Radduso <w.radduso@gmail.com>
2019-09-28 01:26:05 -0300

    - Created a new component to show error messages. - Updated actions to check errors with the request. - Updated Items and Item component to check the errors on the request. - Updated Loading component styles.

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import loader from '../../assets/images/loader.gif';
 import { Col, Row } from "reactstrap";
 
+// Import Components
+import Main from "../Main";
+
 // Import Styles.
 import './styles.scss';
 
 export default () => (
-    <Row>
-        <Col xs="12" className="loader">
-            <img src={loader} alt="Cargando" />
-            <span>Cargando...</span>
-        </Col>
-    </Row>
+    <Main>
+        <Row>
+            <Col xs="12" sm={{ size: 10, offset: 1 }} className="loader marginTop16">
+                <img src={loader} alt="Cargando" />
+                <span>Cargando...</span>
+            </Col>
+        </Row>
+    </Main>
 );
