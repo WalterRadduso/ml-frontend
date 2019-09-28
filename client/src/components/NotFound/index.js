ba9c032a400ab990f684456c7ded01d37aaeca30
Walter Radduso <w.radduso@gmail.com>
2019-09-28 14:39:30 -0300

    - Updated NotFound Component.

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Col, Row } from "reactstrap";
+
+// Import Components
+import Main from "../Main";
+import SearchInput from '../SearchInput';
+
+// Import Images and Styles
+import error404 from '../../assets/images/error_404.png';
+import './styles.scss';
 
 export default () => (
-    <div>
-        404!
-    </div>
+    <React.Fragment>
+        <SearchInput inputSearch={''} />
+
+        <Main>
+            <Row>
+                <Col xs="12" sm={{ size: 10, offset: 1 }} className="error404 marginTop16">
+                    <img src={error404} alt="Error 404"/>
+                    <h6 className="marginTop16">Página no encontrada, intente realizar una búsqueda.</h6>
+                </Col>
+            </Row>
+        </Main>
+    </React.Fragment>
 );
